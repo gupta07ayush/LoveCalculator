4974589db31e6d1cd4f12bae7aa21a1c450029b2
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-03 23:53:32 +0530

    added text entry

--- love_calculator.py
+++ love_calculator.py
@@ -1,22 +1,30 @@
 from tkinter import Tk, Label, Entry
 
 root = Tk()
-root.geometry("500x600")
+root.geometry("500x670")
 root.title("Love Calculator")
 root.config(bg='#6e1423')
 
 heading = Label(root, text="Love Calculator", font=('Times new roman', 40, 'bold'),
-                bg="#85182a", fg='#fdf0d5', border=2, relief='solid')
+                bg="#6a040f", fg='#fdf0d5', border=3, relief='solid')
 heading.place(x=20, y=20, height=100, width=460)
 
 your_name = Label(root, text="Enter Your Name", font=('Arial', 15),
                   bg="#6e1423", fg='black', )
 your_name.place(x=20, y=170, height=30, width=460)
 
+your_name_entry = Entry(root, text="Enter Your Name", font=('Arial', 15),
+                        bg="#6e1423", fg='#fdf0d5', border=10)
+your_name_entry.place(x=70, y=200, height=50, width=360)
+
 
 partner_name = Label(root, text="Enter Your Partner's Name", font=('Arial', 15),
                      bg="#6e1423", fg='black', )
 partner_name.place(x=20, y=350, height=30, width=460)
+
+partner_name_entry = Entry(root, text="", font=('Arial', 15),
+                           bg="#6e1423", fg='#fdf0d5', border=10)
+partner_name_entry.place(x=70, y=380, height=50, width=360)
 
 
 root.mainloop()
