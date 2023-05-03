f1c072a57db540a8928ecbc1fd33b2f705ab3b74
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-03 23:36:17 +0530

    added heading

--- love_calculator.py
+++ love_calculator.py
@@ -1,9 +1,13 @@
-from tkinter import Tk
+from tkinter import Tk, Label, Entry
 
 root = Tk()
 root.geometry("500x600")
 root.title("Love Calculator")
 root.config(bg='#6e1423')
+
+heading = Label(root, text="Love Calculator", font=('Times new roman', 40, 'bold'),
+                bg="#85182a", fg='#fdf0d5', border=2, relief='solid')
+heading.place(x=20, y=20, height=100, width=460)
 
 
 root.mainloop()
