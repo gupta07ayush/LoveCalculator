805112472515e78d8bb2e8daeae60f30a56e4bf2
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-04 00:36:37 +0530

    calculate function

--- love_calculator.py
+++ love_calculator.py
@@ -1,9 +1,36 @@
 from tkinter import Tk, Label, Entry, Button
+import random
 
 root = Tk()
 root.geometry("500x670")
 root.title("Love Calculator")
 root.config(bg='#6e1423')
+
+
+def calculate():
+    name = your_name_entry.get()
+    partner = partner_name_entry.get()
+    if name.lower() == 'prashant' and partner.lower() == 'ananya':
+        result.config(text="98% Match !")
+
+    elif name.lower() == 'ayush' and partner.lower() == 'priyanshi':
+        result.config(text="93% Match !")
+
+    elif name.lower() == 'ashish' and partner.lower() == 'kriti':
+        result.config(text=" -19 %")
+
+    elif name.lower() == 'aman' and partner.lower() == 'bharti':
+        result.config(text="You deserve better.")
+
+    else:
+        if name == '' or partner == '':
+            result.config(text="Error:Enter valid name")
+        else:
+            num = random.randint(50, 80)
+            your_name_entry.delete(first=0)
+            partner_name_entry.delete(first=0)
+            result.config(text=f"{num}% Match!")
+
 
 heading = Label(root, text="Love Calculator", font=('Times new roman', 40, 'bold'),
                 bg="#161a1d", fg='#fdf0d5', border=3, relief='solid')
@@ -27,12 +54,12 @@ partner_name_entry = Entry(root, text="", font=('Arial', 15),
 partner_name_entry.place(x=70, y=350, height=50, width=360)
 
 calculate_button = Button(root, text="Result", font=('Arial', 15, 'bold'),
-                          bg="#6e1423", fg='#fdf0d5', border=5)
+                          bg="#6e1423", fg='#fdf0d5', border=5, command=calculate)
 calculate_button.place(x=150, y=460, height=50, width=200)
 
-result = Label(root, text="", font=('Arial', 15),
+result = Label(root, text="", font=('verdana', 25),
                bg="#6e1423", fg='#fdf0d5', border=2, relief='solid')
-result.place(x=50, y=550, height=70, width=400)
+result.place(x=50, y=550, height=80, width=400)
 
 
 root.mainloop()
