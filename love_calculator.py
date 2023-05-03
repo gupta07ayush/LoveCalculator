93420a7f87f68ec28474b7b88a640d71629e675f
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-03 23:40:33 +0530

    enter your name label

--- love_calculator.py
+++ love_calculator.py
@@ -9,5 +9,9 @@ heading = Label(root, text="Love Calculator", font=('Times new roman', 40, 'bold
                 bg="#85182a", fg='#fdf0d5', border=2, relief='solid')
 heading.place(x=20, y=20, height=100, width=460)
 
+boy_name = Label(root, text="Enter Your Name", font=('Arial', 15),
+                 bg="#6e1423", fg='#fdf0d5', )
+boy_name.place(x=20, y=170, height=30, width=460)
+
 
 root.mainloop()
