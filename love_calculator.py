fb4c0f3ddb32abbf83135c5e78d7df90b94de080
Ayush Gupta <gupta07ayush@gmail.com>
2023-05-03 23:43:20 +0530

    enter your partner's name label

--- love_calculator.py
+++ love_calculator.py
@@ -9,9 +9,14 @@ heading = Label(root, text="Love Calculator", font=('Times new roman', 40, 'bold
                 bg="#85182a", fg='#fdf0d5', border=2, relief='solid')
 heading.place(x=20, y=20, height=100, width=460)
 
-boy_name = Label(root, text="Enter Your Name", font=('Arial', 15),
-                 bg="#6e1423", fg='#fdf0d5', )
-boy_name.place(x=20, y=170, height=30, width=460)
+your_name = Label(root, text="Enter Your Name", font=('Arial', 15),
+                  bg="#6e1423", fg='black', )
+your_name.place(x=20, y=170, height=30, width=460)
+
+
+partner_name = Label(root, text="Enter Your Partner's Name", font=('Arial', 15),
+                     bg="#6e1423", fg='black', )
+partner_name.place(x=20, y=350, height=30, width=460)
 
 
 root.mainloop()
